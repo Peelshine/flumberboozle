94e9e844d1639e99e4a0b8620fff16fe65a22c96
riozap <135473150+Peelshine@users.noreply.github.com>
2024-09-18 23:01:25 +0100

    GRAHHHHHHH I LOST THE GAME

--- parser.py
+++ parser.py
@@ -8,6 +8,12 @@ import codes
 
 
 extension = 'subaru'
+tokens = {
+    'comment_start': '[',
+    'comment_end': ']',
+    'braces_start': '{',
+    'braces_end': '}',
+}
 
 
 
@@ -17,7 +23,11 @@ config_files = glob.glob(f'**/*.{extension}', recursive=True)
 def parse(
 
 ) -> None:
+
     converted_lines = []
+    in_comment = False
+    in_braces = False
+    active_category = ''
 
     for file_name in config_files:
         # Get the current file as a string
@@ -34,6 +44,50 @@ def parse(
                         f'\nLine: {line}')
 
                 # Iterate over every character in the line
+                for character in line:
+
+                    # Handle comments
+                    if (in_comment and
+                    character in tokens['comment_end']
+                    ):
+                        in_comment = False
+                        continue
+
+                    elif (not in_comment and
+                    character in tokens['comment_start']
+                    ):
+                        in_comment = True
+
+                    if in_comment:
+                        continue
+
+
+
+                    # Handle category
+                    #
+                    # Unlike handling comments, it is important we do
+                    # not handle what happens if inside a category
+                    # after determining if inside of a category,
+                    # otherwise the category name will include the
+                    # braces.g
+
+
+                    if (in_braces and
+                    character in tokens['braces_end']
+                    ):
+                        in_category = False
+
+                    if in_braces:
+                        active_category += character
+
+                    elif (not in_braces and
+                    character in tokens['braces_start']
+                    ):
+                        in_braces = True
+
+
+
+                    print(character)
 
                 old_line = line # i go at the end
 
@@ -48,7 +102,7 @@ def write(
 def panic(
     problem
 ) -> None:
-    print(f'{codes.red_bold}PANIC!{codes.reset}'
+    print(f'{codes.red_bold}! PANIC !{codes.reset}'
         f'\n{problem}')
     exit()
 
