9d531a2e737b16f782e590d0eade4c1cb1ac6ddf
riozap <135473150+Peelshine@users.noreply.github.com>
2024-09-25 23:18:22 +0100

    VERY IMPORTANT CHANGE (how do i do branches :sob: is it not working because i need to make a commit?)

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-import window
+import pyglet
+import input
+import update
 
+
+
+RESOLUTION_X = 1920
+RESOLUTION_Y = 1080
+
+
+def close(
+) -> None:
+    print('haha no lol')
 
 
 def main(
 ) -> None:
-    window.init()
+    global window
+    window = pyglet.window.Window()
+
+    label = pyglet.text.Label(
+        'Hello, world!',
+        font_name='Times New Roman',
+        font_size=36,
+        x=window.width//2, y=window.height//2,
+        anchor_x='center', anchor_y='center'
+    )
+
+    @window.event
+    def on_draw():
+        window.clear()
+        label.draw()
+    pyglet.app.run()
+
 
 
 
