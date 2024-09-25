6781478866ec5adc7606021d8bf603aa2da3c668
riozap <135473150+Peelshine@users.noreply.github.com>
2024-09-25 23:22:22 +0100

    what ther sigmer

---
+++ input.py
@@ -0,0 +1,16 @@
+import pyglet
+
+
+
+def on_key_press(
+    symbol,
+    modifiers,
+) -> None:
+    print(symbol,'\n',modifiers)
+
+
+def on_key_release(
+    symbol,
+    modifiers,
+) -> None:
+    print(symbol,'\n',modifiers)
